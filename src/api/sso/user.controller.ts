cb6d1cea0e1cd2a2dd52ec5e0bc03d2ad7d6b66d
ihor <ihor.bielchenko@gmail.com>
2023-02-09 08:56:38 +0100

    test

--- src/api/sso/user.controller.ts
+++ src/api/sso/user.controller.ts
@@ -56,8 +56,6 @@ export class UserController extends HttpController {
 	@Post('verify')
 	async verify(@Body('verifyKey') verifyKey: string): Promise<any> {
 		try {
-			console.log('http', verifyKey);
-
 			return await this.transportService.send({
 				name: this.serviceName, 
 				cmd: `${this.entityName}.verify`,
