f8f9c9ffa6590061bc680e87d45094131ea95299
ihor <ihor.bielchenko@gmail.com>
2023-03-17 17:38:42 +0100

    t

--- @nest-datum/sql/src/sql.service.ts
+++ @nest-datum/sql/src/sql.service.ts
@@ -388,7 +388,10 @@ export class SqlService {
 					? { id: newId }
 					: {},
 			});
-			const output = await this.updateProcess(processedPayload);
+
+			delete processedPayload['newId'];
+
+			const output = await this.updateProcess(payload['id'], { ...processedPayload });
 
 			return await this.updateOutput(processedPayload, await this.updateAfter(payload, processedPayload, output));
 		}
@@ -418,11 +421,11 @@ export class SqlService {
 		return await this.before(payload);
 	}
 
-	protected async updateProcess(payload: object): Promise<any> {
+	protected async updateProcess(id: string, payload: object): Promise<any> {
 		return (utilsCheckObjQueryRunner(this.queryRunner) 
 				&& this.enableTransactions === true)
-			? await this.queryRunner.manager.update(this.entityConstructor, payload['id'], payload)
-			: await this.entityRepository.update({ id: payload['id'] }, payload);
+			? await this.queryRunner.manager.update(this.entityConstructor,id, payload)
+			: await this.entityRepository.update({ id }, payload);
 	}
 
 	protected async updateAfter(initialPayload: object, processedPayload: object, data: any): Promise<any> {
