d866b66f04e5590c73d2dc62cf67c3efc3357665
ihor <ihor.bielchenko@gmail.com>
2023-05-03 09:53:44 +0200

    Test

--- @nest-datum-common/controllers/src/http.controller.ts
+++ @nest-datum-common/controllers/src/http.controller.ts
@@ -259,8 +259,6 @@ export class HttpController extends Controller {
 			if (utilsCheckFunc(err['getHttp'])) {
 				const Exception = err['getHttp']();
 
-				console.log('Exception', Exception);
-
 				throw new Exception(err.message);
 			}
 			throw err;
