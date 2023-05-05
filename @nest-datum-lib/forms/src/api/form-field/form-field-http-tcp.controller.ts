4737f4630a5593f81d50c7cd2d7ce154a5c5f925
ihor <ihor.bielchenko@gmail.com>
2023-05-05 13:27:13 +0200

    Test

--- @nest-datum-lib/forms/src/api/form-field/form-field-http-tcp.controller.ts
+++ @nest-datum-lib/forms/src/api/form-field/form-field-http-tcp.controller.ts
@@ -4,7 +4,7 @@ import { TransportService } from '@nest-datum/transport';
 
 console.log('>>>>>>>>', `${process.env.SERVICE_FORMS}/form/field`)
 
-@Controller(`/form/field`)
+@Controller(`${process.env.SERVICE_FORMS}/form/field`)
 export class FormFieldHttpTcpController extends BindHttpTcpController {
 	protected readonly serviceName: string = process.env.SERVICE_FORMS;
 	protected readonly entityName: string = 'formField';
