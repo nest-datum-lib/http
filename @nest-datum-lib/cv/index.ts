2fe6b75ce32735a3d27a316a0fdb49be0f048ba7
ihor <ihor.bielchenko@gmail.com>
2023-05-10 11:27:46 +0200

    Test

--- @nest-datum-lib/cv/index.ts
+++ @nest-datum-lib/cv/index.ts
@@ -1,11 +1,11 @@
-import { SettingHttpTcpModule } from './api/setting';
-import { AccessAccessOptionHttpTcpModule } from './api/access-access-option';
-import { AccessOptionHttpTcpModule as CvAccessOptionHttpTcpModule } from './api/access-option';
-import { AccessStatusHttpTcpModule as CvAccessStatusHttpTcpModule } from './api/access-status';
-import { AccessHttpTcpModule as CvAccessHttpTcpModule } from './api/access';
-import { RoleAccessHttpTcpModule as CvRoleAccessHttpTcpModule } from './api/role-access';
-import { ReportStatusHttpTcpModule as CvReportStatusHttpTcpModule } from './api/report-status';
-import { ReportHttpTcpModule as CvReportHttpTcpModule } from './api/report';
+import { SettingHttpTcpModule } from './src/api/setting';
+import { AccessAccessOptionHttpTcpModule } from './src/api/access-access-option';
+import { AccessOptionHttpTcpModule as CvAccessOptionHttpTcpModule } from './src/api/access-option';
+import { AccessStatusHttpTcpModule as CvAccessStatusHttpTcpModule } from './src/api/access-status';
+import { AccessHttpTcpModule as CvAccessHttpTcpModule } from './src/api/access';
+import { RoleAccessHttpTcpModule as CvRoleAccessHttpTcpModule } from './src/api/role-access';
+import { ReportStatusHttpTcpModule as CvReportStatusHttpTcpModule } from './src/api/report-status';
+import { ReportHttpTcpModule as CvReportHttpTcpModule } from './src/api/report';
 
 export {
 	CvAccessAccessOptionHttpTcpModule,
