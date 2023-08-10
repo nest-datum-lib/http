1233479b3a1dde1d24d673bd9808f68ea351b7d5
ihor <ihor.bielchenko@gmail.com>
2023-08-10 14:58:34 +0200

    Update model-name controller.

--- @nest-datum/model-name/src/model-name.controller.ts
+++ @nest-datum/model-name/src/model-name.controller.ts
@@ -1,5 +1,7 @@
-import { ExceptionHttpBadRequest } from '@nest-datum/exception-http';
-import { strFilled as utilsCheckStrFilled } from '@nest-datum-utils/check';
+import { 
+	exists as utilsCheckExists,
+	strFilled as utilsCheckStrFilled, 
+} from '@nest-datum-utils/check';
 
 class Sample {
 }
@@ -7,15 +9,33 @@ class Sample {
 export function ModelNameController(Base: any = Sample) {
 	class AbstractBase extends Base {
 		public readonly validateCreateNameIsRequired: boolean;
+		public readonly validateUpdateManyNameIsRequired: boolean;
 
 		async validateCreate(properties: object): Promise<object> {
-			if (this.validateCreateNameIsRequired) {
+			if (this.validateCreateNameIsRequired
+				|| utilsCheckExists(properties['name'])) {
 				if (!(utilsCheckStrFilled(properties['name'])
 					&& properties['name'].length <= 255)) {
-					// throw new (ExceptionHttpBadRequest())(`Name value "${name}" is bad format.`);
+					throw new this.ExceptionBadRequest(`Property "name" "${name}" is bad format.`);
 				}
 			}
 			return await super.validateCreate(properties);
+		}
+
+		async validateUpdateMany(properties: object): Promise<object> {
+			properties = await super.validateUpdateMany(properties);
+
+			if (this.validateUpdateManyNameIsRequired) {
+				let id;
+
+				for (id in properties['body']) {
+					if (utilsCheckExists(properties['body'][id]['name'])
+						&& !utilsCheckStrFilled(properties['body'][id]['name'])) {
+						throw new this.ExceptionBadRequest(`Property "name" "${properties['body'][id]['name']}" is bad format.`);
+					}
+				}
+			}
+			return properties;
 		}
 	}
 
