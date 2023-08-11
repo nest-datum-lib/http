e6d0d3349dc22c4b6771a068ae08f1c4bb9d0178
ihor <ihor.bielchenko@gmail.com>
2023-08-11 10:33:20 +0200

    Update model-name controller.

--- @nest-datum/model-name/src/model-name.controller.ts
+++ @nest-datum/model-name/src/model-name.controller.ts
@@ -10,14 +10,14 @@ export function ModelNameController(Base: any = Sample) {
 	class AbstractBase extends Base {
 		public readonly validateCreateNameIsRequired: boolean;
 		public readonly validateUpdateManyNameIsRequired: boolean;
+		public readonly validateUpdateOneNameIsRequired: boolean;
 
 		async validateCreate(properties: object): Promise<object> {
-			if (this.validateCreateNameIsRequired
-				|| utilsCheckExists(properties['name'])) {
-				if (!(utilsCheckStrFilled(properties['name'])
-					&& properties['name'].length <= 255)) {
-					throw new this.ExceptionBadRequest(`Property "name" "${name}" is bad format.`);
-				}
+			if ((this.validateCreateNameIsRequired
+				|| utilsCheckExists(properties['name']))
+					&& !(utilsCheckStrFilled(properties['name']) 
+						&& properties['name'].length <= 255)) {
+				throw new this.ExceptionBadRequest(`Property "name" "${name}" is bad format.`);
 			}
 			return await super.validateCreate(properties);
 		}
@@ -25,17 +25,26 @@ export function ModelNameController(Base: any = Sample) {
 		async validateUpdateMany(properties: object): Promise<object> {
 			properties = await super.validateUpdateMany(properties);
 
-			if (this.validateUpdateManyNameIsRequired) {
-				let id;
+			let id;
 
-				for (id in properties['body']) {
-					if (utilsCheckExists(properties['body'][id]['name'])
+			for (id in properties['body']) {
+				if ((this.validateUpdateManyNameIsRequired
+					|| utilsCheckExists(properties['body'][id]['name']))
 						&& !utilsCheckStrFilled(properties['body'][id]['name'])) {
-						throw new this.ExceptionBadRequest(`Property "name" "${properties['body'][id]['name']}" is bad format.`);
-					}
+					throw new this.ExceptionBadRequest(`Property "name" "${properties['body'][id]['name']}" is bad format.`);
 				}
 			}
 			return properties;
+		}
+
+		async validateUpdateOne(properties: object): Promise<object> {
+			if ((this.validateUpdateOneNameIsRequired
+				|| utilsCheckExists(properties['name']))
+					&& !(utilsCheckStrFilled(properties['name']) 
+						&& properties['name'].length <= 255)) {
+				throw new this.ExceptionBadRequest(`Property "name" "${name}" is bad format.`);
+			}
+			return await super.validateUpdateOne(properties);
 		}
 	}
 
