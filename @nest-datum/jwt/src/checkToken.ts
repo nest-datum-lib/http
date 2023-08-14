2e8c82f87ac5a8445924d8a584ff5f5dbfacb038
ihor <ihor.bielchenko@gmail.com>
2023-08-14 11:33:30 +0200

    Fix check token.

--- @nest-datum/jwt/src/checkToken.ts
+++ @nest-datum/jwt/src/checkToken.ts
@@ -1,3 +1,4 @@
+import { objFilled as utilsCheckObjFilled } from '@nest-datum-utils/check';
 import utilsJWTCreateHeader from './createHeader';
 import utilsJWTCreatePayload from './createPayload';
 import utilsJWTCreateSignature from './createSignature';
@@ -6,13 +7,17 @@ const checkToken = (token: string, key: string = process.env.JWT_SECRET_ACCESS_K
 	if (!token || token === 'undefined' || token === 'null') {
 		return false;
 	}
-	if (!(payload && typeof payload === 'object')) {
+	if (!utilsCheckObjFilled(payload)) {
 		payload = JSON.parse(String(Buffer.from((String(token).split('.'))[1], 'base64')));
 	}
 	const publicString = `${utilsJWTCreateHeader()}.${utilsJWTCreatePayload(payload)}`;
 	const recoveredToken = `${publicString}.${utilsJWTCreateSignature(publicString.trim(), key)}`;
 
-	return payload;
+	if (token === recoveredToken
+		/*&& (Number(payload['iat']) + Number(payload['exp'])) >= Date.now()*/) {
+		return payload;
+	}
+	return null;
 };
 
 export default checkToken;
